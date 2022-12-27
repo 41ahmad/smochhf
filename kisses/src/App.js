26a91830db1a6f752fb3d7ae1c2ae43070a09e9e
41ahmad <111183178+41ahmad@users.noreply.github.com>
2022-12-27 10:12:20 +0300

    Add files via upload

---
+++ kisses/src/App.js
@@ -0,0 +1,21 @@
+import logo from './IMG_5526.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        </p>
+        <p>
+          𝓢𝓶𝓸𝓸𝓬𝓱 𝓼𝓶𝓸𝓸𝓬𝓱 𝓼𝓶𝓸𝓸𝓬𝓱
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
